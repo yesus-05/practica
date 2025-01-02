7508ab2c7b3292612ff860bcb036385fcf257cdb
Damian23i <damianchavezmurillo@gmail.com>
2025-01-02 16:42:21 -0500

    Damian

--- public/js/eta.js
+++ public/js/eta.js
@@ -9,6 +9,10 @@ try {
     console.log("El catch captura el error")
 }
 
+function suma(numero){
+    return numero % numero
+}
+console.log(suma())
 
 window.onload = function (){
     document.getElementById("todo").classList.add("loaded");
